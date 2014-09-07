974acda42b6f607756308902f1a4df8b9b349bcf
gawkowskimichal <gawkowskimichal@gmail.com>
2014-09-07 16:20:12 +0200

    view

--- View.cpp
+++ View.cpp
@@ -15,26 +15,48 @@ View::View(int argc, char *argv[], Configuration conf) {
 int View::startView(int argc, char *argv[], Configuration conf){
 	std::string GladeFile = conf.getValueByKey("gladeFile");
     Gtk::Main kit(argc, argv);
-    Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
-      try
-      {
-        refBuilder->add_from_file(GladeFile);
-      }
-      catch(const Glib::FileError& ex)
-      {
-    	  std::cout << "FileError: " << ex.what() << std::endl;
-      }
-      catch(const Gtk::BuilderError& ex)
-      {
-    	  std::cout << "BuilderError: " << ex.what() << std::endl;
-      }
-      Gtk::Window window;
-      Gtk::Main::run(window);
-
-      return 0;
+    Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create_from_file(GladeFile);
+    FrmMain *frm = 0;
+    refBuilder->get_widget_derived("menu", frm);
+    kit.run(*frm);
+    return 0;
 }
 
 View::~View() {
 	// TODO Auto-generated destructor stub
+}
+
+
+FrmMain::FrmMain(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade) :
+    Gtk::Window(cobject), builder(refGlade){
+
+    builder->get_widget("btnKalibracja", btnKalibracja);
+    builder->get_widget("btnNagrywanie", btnNagrywanie);
+    builder->get_widget("btnKonfiguracja", btnKonfiguracja);
+    builder->get_widget("btnExit", btnExit);
+
+    btnKalibracja->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_btnKalibracja_clicked));
+    btnNagrywanie->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_btnNagrywanie_clicked));
+    btnKonfiguracja->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_btnKonfiguracja_clicked));
+    btnExit->signal_clicked().connect(sigc::mem_fun(*this, &FrmMain::on_btnExit_clicked));
+}
+
+
+void FrmMain::on_btnExit_clicked(){
+	Gtk::Main::quit();
+}
+
+void FrmMain::on_btnNagrywanie_clicked(){
+
+}
+
+
+void FrmMain::on_btnKonfiguracja_clicked(){
+
+}
+
+
+void FrmMain::on_btnKalibracja_clicked(){
+
 }
 
--- View.h
+++ View.h
@@ -17,4 +17,23 @@ public:
 	virtual ~View();
 };
 
+class FrmMain : public Gtk::Window{
+protected:
+    Glib::RefPtr<Gtk::Builder> builder;
+    Gtk::Button *btnKalibracja;
+    Gtk::Button *btnNagrywanie;
+    Gtk::Button *btnKonfiguracja;
+    Gtk::Button *btnExit;
+
+public:
+    FrmMain(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);//constructor
+
+protected:
+    //signal handlers
+    void on_btnKalibracja_clicked();
+    void on_btnNagrywanie_clicked();
+    void on_btnKonfiguracja_clicked();
+    void on_btnExit_clicked();
+};
+
 #endif /* VIEW_H_ */
